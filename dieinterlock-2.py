c3330ed9f64a16de72abbfa43e760921d80e0f24
PRADOOM RAO <72682151+pradoom@users.noreply.github.com>
2025-04-17 12:07:35 +0530

    Update dieinterlock-2.py

--- dieinterlock-2.py
+++ dieinterlock-2.py
@@ -1,21 +1,22 @@
-import datetime
-import time
 import threading
+from datetime import datetime
+import time
 import serial
 import evdev
 from evdev import categorize, InputDevice, ecodes
 import requests
 import re
-import time
 import json
 import os
 import multiprocessing
 import traceback
+#import logging
+#logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s',filename='InterlokingLog.log',filemode='a')
 
-file_path = "lastCycle.aispl"
-readFileInterval = 10
-interlockThreshold = 10
-
+#file_path = "lastCycle.aispl"
+# readFileInterval = 10
+# interlockThreshold = 10
+UnlockStatus=False
 def DowntimeStatus(username,password,login_url,url):
 
     payload = {
@@ -24,47 +25,41 @@ def DowntimeStatus(username,password,login_url,url):
     }
 
     try:
-       
         login_response = requests.post(login_url, json=payload)
 
         if login_response.status_code in [200, 201]:
             login_data = login_response.json()
-            token = login_data["token"]  
-            print("Login successful")
+            token = login_data["token"]
+            #print("TOKEN:",token)
+        #print("Login successful")
+        #logging.info("Login Successfull")
 
-            headers = {
+        headers = {
                 "Authorization": f"Bearer {token}"
             }
 
+        try:
             response = requests.get(url, headers=headers)
             data = response.json()
-
-            #print("Machine status response:")
-            #print(json.dumps(data, indent=2))  
-
-         
-            if data:
-                downtimeStatus = data[0].get("downtimeStatus")
-                #print("downtime Status:", downtimeStatus)
-                return downtimeStatus
-
-        else:
-            print("Login failed. Status code:", login_response.status_code)
-            print("Response:", login_response.text)
+            return data
+        except Exception as e:
+            print("GET ERROR:",e)
 
     except Exception as e:
-        print("Error:", str(e))
+        print("POST ERROR:", e)
 
 
 
 def readconfig():
-    configfilePath = "/usr/configuration/config/interlocking-config.aispl"
+    configfilePath = "/home/aispl/Interlock/interlocking-config.aispl"
     with open(configfilePath, 'r') as file:
         config = json.load(file)
     file_path = config['file_path']
     readFileInterval = config['readFileInterval']
     interlockThreshold = config['interlockThreshold']
-    return file_path, readFileInterval, interlockThreshold
+    LockCondation = config['LockCondation']
+    machineId_=config['machineId']
+    return file_path, readFileInterval, interlockThreshold, LockCondation,machineId_
 
 def send_signal(value):
     data = {"I1": value, "I2": value}
@@ -110,26 +105,35 @@ def convert_to_json(data):
     return die_dict
 
 
-def decode_barcode_data(data):
-    parts = data.split('LEFTSHIFT')
-    joined_string = ''.join(parts)
-    decoded_string = joined_string.replace('SEMICOLON', ':').replace('COMMA', ',').replace('MINUS', '-').replace('ENTER', '\n').replace('\n','')
-    return decoded_string
-
-def find_barcode_scanner():
+def Find_BarCodeScanner():
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    
     for device in devices:
-        # print(f"Checking Device: {device.path}, Name: {device.name}")
-        # Check if the device name contains common barcode scanner identifiers
         if 'barcode' in device.name.lower() or 'scanner' in device.name.lower() or 'hid' in device.name.lower():
             return device.path
-    return None
+        else:
+            return 0
+
+#MachineID :M-VIC-TOOL-0267
+#DieID :D-DIEID:WMA0022,OPERATIONID:WMA0022-10,CAVITY:1
+
+def StringToDict(data):
+    list1=data.split(",")
+    #print(list1)
+    dict={}
+    for pair in list1:
+        key, value = pair.split(':', 1)
+        key = key.strip()
+        value = value.strip()
+        dict[key] = value
+
+    return dict
 
 def read_timestamp_and_value(file_path):
     try:
         with open(file_path, 'r') as file:
-            data = json.load(file)  # Load JSON as dictionary
-            for timestamp_str, value in data.items():  # Get the first (or only) item
+            data = json.load(file)
+            for timestamp_str, value in data.items():
                 timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S.%f')
                 return timestamp, value
     except Exception as e:
@@ -137,149 +141,215 @@ def read_timestamp_and_value(file_path):
         return None, None
 
 
-def read_file(file_path,readFileInterval , interlockThreshold,LockCondation):
+def read_file(file_path,readFileInterval,interlockThreshold,LockCondation,mId):
+    global UnlockStatus
     # Login credentials and machine ID
     login_url = "https://pel.quadworld.in/auth/login"
-    username = "vipl@aispl.co"
-    password = "Autosys@9870"
-    machineId = "VIC-TOOL-0267"
-
+    username = "Autosys@aispl.co"
+    password = "Autosys@123"
+    machineId = mId
+    locked= False
     # Machine status API URL
     url = f"https://pel.quadworld.in/client/location/plant/division/line/machines?downtimeStatus=0&id={machineId}"
 
-    
+    if(LockCondation==""):
+        print("ERROR:Assign Lockcondation in config")
+        return
+
+
     while True:
         if LockCondation == "Punch":
 
-            print("INTER: Loop is running Condation = PUNCH...")
+            print("DEBUG: LockCondition = PUNCH")
+            #logging.info("DEBUG: Loop is running, Condation = PUNCH")
 
-            if os.path.exists(file_path):
+            try:
+                if os.path.exists(file_path):
 
-                file_timestamp, value = read_timestamp_and_value(file_path)
+                    file_timestamp, value = read_timestamp_and_value(file_path)
 
-                time_gap = (datetime.datetime.now() - file_timestamp).total_seconds()
+                    time_gap = (datetime.now() - file_timestamp).total_seconds()
 
-                if not locked and time_gap > interlockThreshold * 60:
+                    if not locked and time_gap > interlockThreshold * 60:
 
-                    print("INTER: Time gap exceeded...")
+                        print("DEBUG: Time gap exceeded...")
+                        #logging.info("DEBUG: Time gap exceeded.."
 
-                    downtimeStatus = DowntimeStatus(username, password, login_url, url)
+                        downtimeStatus = DowntimeStatus(username, password, login_url, url)
 
-                    print("INTER: Downtime Status:", downtimeStatus)
+                        #print("DEBUG: Downtime Status:", downtimeStatus)
 
-                    if downtimeStatus is not None:
+                        if downtimeStatus:  # downtimeStatus is true
 
-                        if downtimeStatus:  # downtimeStatus is truthy (non-zero)
-
-                            print("INTER: Reason NOT punched")
-
+                            print("DEBUG: Reason NOT punched")
+                            #logging.info("DEBUG: Reason NOT punched")
                             send_signal("1")
 
                             locked = True
                         else:
-                            print("INTER: Reason punched")
-                            
+                            print("DEBUG: Reason punched")
+                            #logging.info("DEBUG: Reason punched")
                             send_signal("0")
-                    else:
-                        print("INTER: Could not fetch downtime status")
+                            current_time=datetime.now()
+                            with open(file_path, 'w') as file:
+                                file.write(str(json.dumps({current_time.strftime('%Y-%m-%d %H:%M:%S.%f'):0})))
 
-                elif locked:
-                    downtimeStatus = DowntimeStatus(username, password, login_url, url)
-                    print("INTER: Locked check - Downtime Status:", downtimeStatus)
+                            locked = False
 
-                    if downtimeStatus is not None and not downtimeStatus:
-                        send_signal("0")
-                        locked = False
-                        print("INTER: Reason punched, unlocking")
-                    else:
-                        print("INTER: Reason NOT punched -- Machine still locked")
-            else:
-                print("INTER: File not found")
+                    elif locked:
+                        downtimeStatus = DowntimeStatus(username, password, login_url, url)
+                        #print("DEBUG: Locked check - Downtime Status:", downtimeStatus)
 
-            time.sleep(readFileInterval)
+                        file_timestamp, value = read_timestamp_and_value(file_path)
+
+                        time_gap = (datetime.now() - file_timestamp).total_seconds()
+
+                        if not downtimeStatus or UnlockStatus==True:
+                            send_signal("0")
+                            locked = False
+                            
+                            UnlockStatus=False
+                            print("DEBUG: Reason punched, unlocking")
+                            if not downtimeStatus:
+                                print("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+                                current_time=datetime.now()
+                                with open(file_path, 'w') as file:
+                                    file.write(str(json.dumps({current_time.strftime('%Y-%m-%d %H:%M:%S.%f'):0})))
+                            #logging.info("DEBUG: Reason punched, unlocking")
+                        else:
+                            print("DEBUG: Reason NOT punched -- Machine still locked")
+                            #logging.info("DEBUG: Reason NOT punched -- Machine still locked")
+            except Exception as e:
+                print("FILE ERROR:",e)
+                #logging.debug("FILE ERROR:",e)
+
+
         elif LockCondation == "None":
-            print("INTER: Loop is running Condation = NOne...")
+            print("DEBUG: LockCondition = None")
             if os.path.exists(file_path):
                 file_timestamp, value = read_timestamp_and_value(file_path)
-                time_gap = (datetime.datetime.now() - file_timestamp).total_seconds()
+                time_gap = (datetime.now() - file_timestamp).total_seconds()
 
-                if time_gap > interlockThreshold * 60:
+                if(time_gap > interlockThreshold * 60):
                     print("Machine LOCK")
                     send_signal("1")
+                    
+                    current_time=datetime.now()
+                    with open(file_path, 'w') as file:
+                        file.write(str(json.dumps({current_time.strftime('%Y-%m-%d %H:%M:%S.%f'):0})))
+                        #print("time Reset in file", current_time)
+
                     #This condation FaIl only when right diY is AssIgn.
+        else:
+            print("Invalid Lockcondition assign in config!")
+            #logging.info("Invalid Lockcondation assign in config!")
 
-def read_barcode_data(file_path,interval , threshold):
-    print("Started barcode reading")
-    device_path = find_barcode_scanner()
-    if not device_path:
-        print("No barcode scanner detected. Please check the connections.")
-        return
+        time.sleep(readFileInterval)
 
-    try:
-        device = InputDevice(device_path)
-        print("Listening to events from: {}".format(device.path))
-        print("Scan a barcode to see the events. Press Ctrl+C to stop.\n")
+def read_barcode_data(file_path,interval,LockCondation,mId):
+
+    global UnlockStatus
+    # Login credentials and machine ID
+    login_url = "https://pel.quadworld.in/auth/login"
+    username = "Autosys@aispl.co"
+    password = "Autosys@123"
+    machineId = mId
+    # Machine status API URL
+    url = f"https://pel.quadworld.in/client/location/plant/division/line/machines?downtimeStatus=0&id={machineId}"
 
 
-        datadict = {}
-        barcode_data = ''
-        pattern = r"^[A-Z]+-[A-Z]+-\d{4}$"
-        url = "http://dal.aispl.co:3030/die-logs"
-
-        for event in device.read_loop():
+    urldie = "http://dal.aispl.co:3030/die-logs"
+    PostDict={}
+    barcode_data = ''
+    device=Find_BarCodeScanner()
+    if device!=0:
+        device_path = InputDevice(device)      
+        for event in device_path.read_loop():
             if event.type == ecodes.EV_KEY:
                 key_event = categorize(event)
-                if key_event.keystate == key_event.key_down:
-                    barcode_data += ecodes.KEY[event.code].replace('KEY_', '')
+                #print(key_event)    
+                if key_event.keystate == key_event.key_down:  
+                    barcode_data += ecodes.KEY[event.code].replace('KEY_','')
+                    #print(barcode_data)   
                     if event.code == ecodes.KEY_ENTER:
-                        decoded_data = decode_barcode_data(barcode_data)
+                        
+                        data=("".join(barcode_data.split("LEFTSHIFT"))).replace('SEMICOLON', ':').replace('COMMA', ',').replace('MINUS', '-').replace('ENTER', '\n').replace('\n','').replace('SLASH','/').replace('SPACE','')
+                        print(data)
+                        if(data[0]=="D"):
+                            #print("D:",data)
+                            TempDict=StringToDict(data)
+                            PostDict.update({"dieId":TempDict["D-DIEID"],"operationId":TempDict["OPERATIONID"]})
+                            print("DieID:",PostDict)
+                          
+                        elif(data[0]=="M" or re.match(r"^[A-Z]+-[A-Z]+-\d{4}$",data)):
+                            #print("M:",data)
+                            #data=data.split()
+                            #print(len(data))
+                            PostDict.update({"machineId":data})
+                            print("machineID:",PostDict)
+                           
+                        elif(data=="POST"):
+                            print("Data POST")
 
-                        print("Decoded Barcode Data:", decoded_data)
-                        print(decoded_data[0], "DDDDDDDDDDDDDDDDDDDDDDDDDD")
-                        print(datadict,"datadict.update")
-                        if decoded_data[0] == "D": 
-                            die_dict = convert_to_json(decoded_data)
-                            datadict.update(
-                                {
-                                    "dieId":die_dict["D-DIEID"],
-                                    "operationId":die_dict["OPERATIONID"]
-                                })
-                        elif decoded_data[0] == "M" or re.match(pattern, decoded_data):
-                            datadict.update({"machineId":decoded_data[-13:]}) 
+                            if "dieId" in PostDict and "machineId" in PostDict:
 
-                        elif decoded_data == "POST":
-                            print("EEEEE")
-                            if "machineId" in datadict and "dieId" in datadict:
-                                print("OOOOOOO")
-                                response = call_api(url, method="POST",jsondata=datadict)
-                                response = json.loads(response.text)
-                                print("responseeee------------------" ,   response)
-                                if response["linked"] == True:
-                                    print("true")
-                                    result = send_signal('0')
-                                    print(result, " resukt of serial write on relay UNLOCK")
-                                elif response["linked"] == False:
-                                    print("true")
+                                #print("DieID:",len(PostDict["dieId"]))
+                                #print("Machine",len(PostDict["machineId"]))
+
+                                try:
+                                    status=requests.post(urldie,data=PostDict)
+                                    response = json.loads(status.text)
+
+                                except Exception as e:
+                                    print("POST ERROR:",e)
+                            
+                                if(response["linked"]==True):
+                                    print("response:",response["linked"])
+                                    if(LockCondation == "None"):
+                                        #downtimeStatus = DowntimeStatus(username, password, login_url, url)
+                                        if(not DowntimeStatus(username, password, login_url, url)):
+                                            print("#####------------Die link and downtimebutton punched!-----------#####")
+                                            send_signal("0")
+                                    elif(LockCondation == "Punch"):
+                                        
+                                        UnlockStatus=True
+                                        send_signal("0")
+                                        current_time=datetime.now()
+                                        with open(file_path, 'w') as file:
+                                            file.write(str(json.dumps({current_time.strftime('%Y-%m-%d %H:%M:%S.%f'):0})))
+                                            print("time Reset in file", current_time)
+                                    else:
+                                        print("Assigned Proper Condition : Punch/None in Config!")
+                                else:
                                     send_signal("1")
-                                datadict = {}
+                                    print("response:",response["linked"])
+
                             else:
-                                print("machineID/dieID not found in datadict")
-                                datadict = {}
+                                print("Machine ID/Die Id Not Scan!")
+
+
                         barcode_data = ''
+                        #data=''
+
                     
-    except KeyboardInterrupt:
-        print("Stopped by user.")
+    else:
+        print("Barcode Device Not Found")
+        #logging.info("Barcode Device not Found")
+
+
+if __name__ == "__main__":
+    
+    datetimeFile, interval, threshold, Lockcondation,mId = readconfig()
+    try:
+        t1 = threading.Thread(target=read_barcode_data, args=(datetimeFile, interval,Lockcondation,mId))
+        t2 = threading.Thread(target=read_file, args=(datetimeFile, interval, threshold, Lockcondation,mId))
+        t1.start()
+        t2.start()
+        #t1.join()
+        #t2.join()
     except Exception as e:
-        # print(f"An error occurred barcode: {e}")
-        pass
+        print("Thread ERROR: ",e)
+
+    print("Both threads started.-----------------")
 
 
-if __name__ =="__main__":
-    datetimeFile  , interval , threshold  = readconfig()
-    t1 = multiprocessing.Process(target=read_barcode_data, args=(datetimeFile,interval , threshold))
-    t2 = multiprocessing.Process(target=read_file, args=(datetimeFile,interval , threshold))
-    t1.start()
-    t2.start()
-    # t1.join()
-    # t2.join()
-    print("Done!")
